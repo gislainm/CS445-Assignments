6af3239970a01a1846ad8c25d54e3db090609d86
gislainm <gmuhikira2@gmail.com>
2022-05-01 14:37:28 -0500

    Design Pattern 2

--- w1d5/assignment3.js
+++ w1d5/assignment3.js
@@ -31,23 +31,23 @@ const shoppingCart = (
     }
 )()
 
-const item1 = { id: 0, product: { id: 1, name: 'Coffee', description: 'Coffee Grounds from Ethiopia', price: 9 }, count: 1 }
-const item2 = { id: 1, product: { id: 2, name: 'Tea', description: 'Oonlong Tea from China', price: 10 }, count: 5 }
-const item3 = { id: 2, product: { id: 3, name: 'Bottled Water', description: 'Bottled Water from US', price: 2 }, count: 30 }
+// const item1 = { id: 0, product: { id: 1, name: 'Coffee', description: 'Coffee Grounds from Ethiopia', price: 9 }, count: 1 }
+// const item2 = { id: 1, product: { id: 2, name: 'Tea', description: 'Oonlong Tea from China', price: 10 }, count: 5 }
+// const item3 = { id: 2, product: { id: 3, name: 'Bottled Water', description: 'Bottled Water from US', price: 2 }, count: 30 }
 
-shoppingCart.upsertItem(item1);
-shoppingCart.upsertItem(item2);
-shoppingCart.upsertItem(item3);
-console.log(shoppingCart.getTotalPrice()); //Expected Result: 119
-item3.product.name = 'Himilayan Water';
-item3.product.price = 10;
-shoppingCart.upsertItem(item3);
+// shoppingCart.upsertItem(item1);
+// shoppingCart.upsertItem(item2);
+// shoppingCart.upsertItem(item3);
+// console.log(shoppingCart.getTotalPrice()); //Expected Result: 119
+// item3.product.name = 'Himilayan Water';
+// item3.product.price = 10;
+// shoppingCart.upsertItem(item3);
 
-console.log(shoppingCart.getItemsCount()); //Expected Result: 3
-console.log(shoppingCart.getTotalPrice()); //Expected Result: 359
-shoppingCart.removeItemById(1);
-console.log(shoppingCart.getItemsCount()); //Expected Result: 2
-console.log(shoppingCart.getTotalPrice()); //Expected Result: 309
+// console.log(shoppingCart.getItemsCount()); //Expected Result: 3
+// console.log(shoppingCart.getTotalPrice()); //Expected Result: 359
+// shoppingCart.removeItemById(1);
+// console.log(shoppingCart.getItemsCount()); //Expected Result: 2
+// console.log(shoppingCart.getTotalPrice()); //Expected Result: 309
 
 
 //QUESTION 2
@@ -71,21 +71,21 @@ class Subject {
     }
 }
 
-const subject = new Subject();
-subject.on('eat', console.log); // register an observer
-subject.on('study', console.log); // register an observer
+// const subject = new Subject();
+// subject.on('eat', console.log); // register an observer
+// subject.on('study', console.log); // register an observer
 
-function foo(msg) {
-    console.log('foo: ' + msg);
-}
-subject.on('eat', foo);
-subject.on('study', foo);
+// function foo(msg) {
+//     console.log('foo: ' + msg);
+// }
+// subject.on('eat', foo);
+// subject.on('study', foo);
 
-subject.emit('eat', 'Corn');
-//output for Line above: subject.emit('eat', 'Corn');
-// Corn
-// foo: Corn
-subject.emit('study', 'cs445');
-//output for Line above: subject.emit('study', 'cs445');
-// cs445
-// foo: cs445
+// subject.emit('eat', 'Corn');
+// //output for Line above: subject.emit('eat', 'Corn');
+// // Corn
+// // foo: Corn
+// subject.emit('study', 'cs445');
+// //output for Line above: subject.emit('study', 'cs445');
+// // cs445
+// // foo: cs445
---
+++ w1d5/assignment4.js
@@ -0,0 +1,144 @@
+"use strict";
+/*eslint-disable */
+
+//QUESTION 1
+class Regular {
+    constructor(type) {
+        this.bulb = "regular";
+    }
+}
+
+class Energy {
+    constructor(type, color) {
+        this.bulb = type;
+        this.color = color;
+    }
+}
+
+class Factory {
+    createBulb(type, color) {
+        let bulb;
+        if (type === "regular") {
+            bulb = new Regular(type);
+        } else if (type === "energy") {
+            bulb = new Energy(type, color);
+        }
+        return bulb;
+    }
+}
+
+// const bulbs = [];
+// const factory = new Factory();
+// bulbs.push(factory.createBulb("regular"));
+// bulbs.push(factory.createBulb("energy", "red"));
+// for (let i = 0, len = bulbs.length; i < len; i++) {
+//     console.log(bulbs[i]);
+// }
+
+
+//QUESTION 2
+class User {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+class DecoratedUser {
+    constructor(user, city, state) {
+        this.user = user;
+        this.name = user.name;
+        this.city = city;
+        this.state = state;
+    }
+    logger() {
+        console.log(this.name + " is from the city of " + this.city + " in the state of " + this.state);
+    }
+}
+
+// const user = new User("Kelly");
+// const decorated = new DecoratedUser(user, "Broadway", "New York");
+// decorated.logger();
+
+
+//QUESTION 3
+class Info {
+    logging(message) {
+        console.info(message);
+    }
+}
+
+class Warn {
+    logging(message) {
+        console.warn(message);
+    }
+}
+
+class Error {
+    logging(message) {
+        console.error(message);
+    }
+}
+
+class Table {
+    logging(message) {
+        console.table(message);
+    }
+}
+
+class Strategy {
+    setStrategy(loggingAlgo) {
+        this.loggingAlgo = loggingAlgo;
+    }
+    logging(message) {
+        this.loggingAlgo.logging(message);
+    }
+}
+// const strategy = new Strategy();
+
+// strategy.setStrategy(new Info());
+// strategy.logging('info....');
+
+// strategy.setStrategy(new Warn());
+// strategy.logging('warn....');
+
+// strategy.setStrategy(new Error());
+// strategy.logging('error....');
+
+// strategy.setStrategy(new Table());
+// strategy.logging(['table', 'table']);
+
+
+//QUESTION 4
+const fibonacci = (function () {
+    let memo = {};
+    function fibo(n) {
+        let value;
+        if (n in memo) {
+            value = memo[n];
+        } else {
+            if (n === 0 || n === 1) {
+                value = n;
+            } else {
+                value = fibo(n - 1) + fibo(n - 2);
+            }
+            memo[n] = value
+        }
+        return value;
+    }
+    return fibo;
+})()
+
+// function fibonacci2(n) {
+//     if (n <= 1) {
+//         return n;
+//     }
+//     return fibonacci2(n - 1) + fibonacci2(n - 2);
+// }
+
+// console.time('Memoized Version');
+// console.log(fibonacci(50));
+// console.timeEnd('Memoized Version');
+
+// console.time('Not Memoized Version');
+// console.log(fibonacci2(50));
+// console.timeEnd('Not Memoized Version');
