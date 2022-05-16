c2b2c429d7a81270e2cffb5fe564c26046dcc5e2
gislainm <gmuhikira2@gmail.com>
2022-05-15 22:16:50 -0500

    final project third commit

--- finalProject/myPages/main.js
+++ finalProject/myPages/main.js
@@ -32,6 +32,7 @@ window.onload = function () {
         document.getElementById("mySongs").innerHTML = null;
         notLoggedIn();
     }
+    document.getElementById("searchBtn").onclick = search;
 }
 
 function notLoggedIn() {
@@ -69,6 +70,7 @@ function loggedInFeatures(data) {
 }
 
 function fetchMusic() {
+    document.getElementById("songList").innerHTML = null;
     fetch(`${SERVER_ROOT}/api/music`, {
         headers: {
             'Authorization': `Bearer ${localStorage.getItem('accessToken')}`
@@ -105,7 +107,9 @@ function fetchPlaylist() {
             let mySongs = document.getElementById("mySongs");
             if (!songs.length) {
                 document.getElementById("noPlaylist").style.display = 'block';
+                document.getElementById("playlist").style.display = 'none';
             } else {
+                document.getElementById("noPlaylist").style.display = 'none';
                 document.getElementById("playlist").style.display = 'block'
                 songs.forEach((song) => {
                     mySongs.innerHTML += `<tr>
@@ -154,5 +158,43 @@ function removeToPlaylist(obj) {
 
 function playMusic() {
     let songName = document.getElementById("searchBar").value;
+}
+function search() {
+    let songToSearch = document.getElementById("searchBar").value;
+    let songList = document.getElementById("songList");
+    if (!songToSearch) {
+        afterLogIn();
+        document.getElementById("noSongFound").style.display = "none";
+    } else {
+        songList.innerHTML = null;
+        fetch(`${SERVER_ROOT}/api/music/?search=${songToSearch}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        }).then(response => response.json())
+            .then(data => {
+                if (data.length == 0) {
+                    document.getElementById("noSongFound").style.display = "block";
+                } else {
+                    let count = 1;
+                    data.forEach(song => {
+                        songList.innerHTML += `<tr>
+                        <th scope="row">${count}</th>
+                        <td>${song.title}</td>
+                        <td>${song.releaseDate}</td>
+                        <td><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill" id="addBtn" data-music="${song.id}" onclick="addToPlaylist(this)" viewBox="0 0 16 16">
+                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
+                      </svg> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-circle-fill" id="playBtn" viewBox="0 0 16 16">
+                      <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z"/>
+                    </svg></td>
+                    </tr>`;
+                        count++;
+                    })
+                }
+            });
+    }
+    document.getElementById("searchBar").value = "";
 }
 
